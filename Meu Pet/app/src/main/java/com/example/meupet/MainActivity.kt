8d3113996be6af8ef0f2c3d87f1dd6a8064af4b1
ChristalCamillo <christal.b.camillo@gmail.com>
2022-08-31 16:40:11 -0300

    finalizado app meuPet

--- Meu Pet/app/src/main/java/com/example/meupet/MainActivity.kt
+++ Meu Pet/app/src/main/java/com/example/meupet/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.meupet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sendMessage(view: View) {Toast.makeText(this, "Oi meu nome é biscoito", Toast.LENGTH_LONG).show()}
 }
